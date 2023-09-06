599b747465e41aab026300c76281ac9f16c948ae
Sachinkumar Gupta <143322866+Sachingupta214@users.noreply.github.com>
2023-09-06 15:15:03 +0000

    let var const learn done

---
+++ 01_basics/variables.js
@@ -0,0 +1,11 @@
+const accountId = 1432123
+let accountEmail = "Sachin@google@gmail.com"
+var accountPassword = "Sachin@1234"
+accountCity = "Mumbai"
+
+// is just practice 
+accountEmail = "hdfc@gmail.com"
+accountPassword = "Sachin"
+accountCity = "Titwala"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
