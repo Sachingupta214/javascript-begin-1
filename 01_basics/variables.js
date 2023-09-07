2b493a44a61da73ec731d805611169b8ec41f69d
Sachinkumar Gupta <143322866+Sachingupta214@users.noreply.github.com>
2023-09-07 08:19:02 +0000

    datatype done

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -2,10 +2,16 @@ const accountId = 1432123
 let accountEmail = "Sachin@google@gmail.com"
 var accountPassword = "Sachin@1234"
 accountCity = "Mumbai"
+let accountState;
 
 // is just practice 
 accountEmail = "hdfc@gmail.com"
 accountPassword = "Sachin"
 accountCity = "Titwala"
 
-console.table([accountId, accountEmail, accountPassword, accountCity])
+
+// {}curly bracket we called ----- Scope 
+// prefer not to use var 
+// because there are issue of block scope and functional scope issues 
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
