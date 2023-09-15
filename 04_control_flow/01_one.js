0480e4591bc08e183e2f5a72b3912ca8c5af175d
Sachinkumar Gupta <143322866+Sachingupta214@users.noreply.github.com>
2023-09-15 08:50:18 +0000

    ok

--- 04_control_flow/01_one.js
+++ 04_control_flow/01_one.js
@@ -1,9 +1,49 @@
 //  if
 
-const isUserLoggedIn = true
+// const isUserLoggedIn = true
+// const tempreture = 41
 
-if (2==2){
-    console.log('executed');
-}
+// if (tempreture === 49){
+//     console.log('less than 50');
+// } else{
+// console.log('tempreture is Greater Than 50');
+// }
+// console.log('executed');
+
+
+// const score = 20
+
+// if (score > 100){
+//     var power = "fly"
+//     console.log(`User power: ${power}`)
+// }
+
+// console.log(`User power: ${power}`);
 
 // <, >, <=, >= , ==, !=, ===, !==
+
+const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2");
+
+// if (balance < 500) {
+//     console.log("less than 500")
+// } else if (balance < 750) {
+//     console.log("less tthan 750")
+// }else if (balance < 950) {
+//     console.log("less than 950")
+// }else {
+//     console.log("less than 1200")
+// }
+
+const isUserLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if(isUserLoggedIn && debitCard  && 2==3){
+    console.log("Allow to buy course")
+}
+fi(user){
+    console.log()
+}
