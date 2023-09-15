87e52681e7d28a2623b4380e24915a2dcdf40fca
Sachinkumar Gupta <143322866+Sachingupta214@users.noreply.github.com>
2023-09-15 06:27:35 +0000

    ok

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -8,8 +8,38 @@ const user = {
     }
 }
 
-user.welcomeMessage()
-user.username = "sam"
-user.welcomeMessage()
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
 
-console.log(this);
+// console.log(this);
+
+// function chai() {
+//     let username: "sachin"
+    // console.log(this.username);
+// }
+
+// chai()
+
+// const chai = () => {
+//     let username = "Sachin"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) => (num1+num2)
+
+// const addTwo = (num1, num2) => num1 + num2
+
+// const addTwo = (num1, num2) => ({username: "Hitesh"})
+
+// console.log(addTwo(3,5));
+
+const myArray = [2,3,5,7,8]
+
+myArray.forEach()
